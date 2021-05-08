a9d2aab8edfba3bc23d43600d290d2149e9655b5
Peter Smith <psmithsf@gmail.com>
2021-05-08 15:09:38 -0700

    added styles to modal component

--- resources/js/Components/VTailwindModal.vue
+++ resources/js/Components/VTailwindModal.vue
@@ -2,6 +2,7 @@
   <vue-final-modal
     v-slot="{ params, close }"
     v-bind="$attrs"
+    :esc-to-close="true"
     classes="flex justify-center items-center"
     content-class="relative flex flex-col max-h-full mx-4 p-4 border dark:border-gray-800 rounded bg-white dark:bg-gray-900"
   >
@@ -17,10 +18,11 @@
     </div>
     <button class="absolute top-0 right-0 mt-2 mr-2" @click="close">
       <!-- <mdi-close></mdi-close> -->
-      CLOSE
+    <svg focusable="false" width="2em" height="2em" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41z" fill="currentColor"></path></svg>
     </button>
   </vue-final-modal>
 </template>
+
 
 <script>
 export default {
@@ -30,6 +32,32 @@ export default {
 }
 </script>
 
+
 <style>
+/* give the buttons a button-y look */
+button.vfm-btn {
+    padding: .25rem .5rem;
+    border-width: 1px;
+    border-radius: .25rem;
+}
+
+/* put some space on left side of second/side-by-side button */
+button.vfm-btn+button.vfm-btn {
+    margin: 0 0 0 1rem;
+}
+
+html{
+    font-family: DM Sans,system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,BlinkMacSystemFont,Helvetica Neue,Arial,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;
+}
+
+p {
+    display: block;
+    margin-top: 1.25em;
+    margin-bottom: 1.25em;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+}
 </style>
 
